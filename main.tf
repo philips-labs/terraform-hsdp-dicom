a9350d1cf359584684cdb8a49a119ea7d54e68fc
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-04-05 11:38:12 +0200

    Iterate over s3creds_credentials list items

--- main.tf
+++ main.tf
@@ -27,33 +27,31 @@ resource "hsdp_dicom_store_config" "svc_cdr" {
 }
 
 resource "hsdp_dicom_object_store" "s3creds_store" {
-  count           = var.iam_org_id != "" ? 1 : 0
+  count           = length(var.s3creds_credentials)
   config_url      = hsdp_dicom_store_config.config.config_url
   organization_id = var.iam_org_id
   description     = "S3Creds Object Store - Terraform managed"
 
   s3creds_access {
     endpoint    = lookup(var.s3creds_bucket_endpoint, var.region)
-    product_key = var.s3creds_product_key
-    bucket_name = var.s3creds_bucket_name
-    folder_path = "/${var.iam_org_id}/"
+    product_key = var.s3creds_credentials[count.index].product_key
+    bucket_name = var.s3creds_credentials[count.index].bucket_name
+    folder_path = "/${var.iam_org_id}/" # TODO: is this the right IAM id?
     service_account {
-      service_id  = hsdp_iam_service.svc_dicom_s3creds.service_id
-      private_key = hsdp_iam_service.svc_dicom_s3creds.private_key
-      # Todo: replace
-      access_token_endpoint = "${var.iam_url}/oauth2/access_token"
-      token_endpoint        = "${var.iam_url}/oauth2/access_token"
-      # Todo: Make name as option field
-      name = "Terraform Managed"
+      service_id            = var.s3creds_credentials[count.index].service_id
+      private_key           = var.s3creds_credentials[count.index].private_key
+      access_token_endpoint = "${data.hsdp_config.iam.url}/oauth2/access_token"
+      token_endpoint        = "${data.hsdp_config.iam.url}/oauth2/access_token"
+      name                  = "Terraform Managed"
     }
   }
 }
 
 resource "hsdp_dicom_repository" "s3creds_repository" {
-  count                      = var.iam_org_id != "" ? 1 : 0
+  count                      = length(var.s3creds_credentials)
   config_url                 = hsdp_dicom_store_config.config.config_url
-  repository_organization_id = var.repository_organization_id
-  organization_id            = var.iam_org_id
+  repository_organization_id = var.s3creds_credentials[count.index].repository_organization_id
+  organization_id            = var.s3creds_credentials[count.index].repository_organization_id # TODO: Is this right?
   object_store_id            = hsdp_dicom_object_store.s3creds_store[count.index].id
 }
 
