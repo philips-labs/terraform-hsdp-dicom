c15aca15bfb59b3cd168f2002dd82461eaaf43f7
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-05-12 15:52:20 +0200

    Fix typo

--- modules/dedicated/main.tf
+++ modules/dedicated/main.tf
@@ -4,7 +4,7 @@ resource "hsdp_iam_proposition" "prop_dicom" {
   organization_id = var.organization_id
 }
 
-resource "hsdp_iam_application" "app_diom" {
+resource "hsdp_iam_application" "app_dicom" {
   name           = "APP_DICOM_TF"
   description    = "APP_DICOM_TF - Terraform managed"
   proposition_id = hsdp_iam_proposition.prop_dicom.id
@@ -33,7 +33,7 @@ resource "hsdp_iam_group" "grp_dicom_cdr" {
 resource "hsdp_iam_service" "svc_dicom_cdr" {
   name           = "SVC_DICOM_CDR_TF"
   description    = "SVC_DICOM_CDR_TF - Terraform managed"
-  application_id = hsdp_iam_application.app_diom.id
+  application_id = hsdp_iam_application.app_dicom.id
   validity       = 36
   scopes         = ["openid"]
   default_scopes = ["openid"]
@@ -72,7 +72,7 @@ resource "hsdp_iam_role" "role_dicom_s3creds" {
 resource "hsdp_iam_service" "svc_dicom_s3creds" {
   name           = "SVC_DICOM_S3CREDS_TF"
   description    = "SVC_DICOM_S3CREDS_TF - Terraform managed"
-  application_id = hsdp_iam_application.app_diom.id
+  application_id = hsdp_iam_application.app_dicom.id
   validity       = 36
   scopes         = ["openid"]
   default_scopes = ["openid"]
